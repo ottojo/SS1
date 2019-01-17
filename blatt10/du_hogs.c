b82096c8c4d13a7128dfca5a73c9b4655dfb26db
ottojo <jottosmail@gmail.com>
2019-01-17 13:57:18 +0100

    "fix" usage

--- blatt10/du_hogs.c
+++ blatt10/du_hogs.c
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
         if (argv[1][0] == '-') {
             // Only number supplied
             if (argv[1][1] == '?') {
-                fprintf(stderr, "Usage: ./du_hogs [-N] [dir]\n");
+                fprintf(stderr, "Usage: %s [-N] [dir]\n", argv[0]);
                 exit(EXIT_FAILURE);
             }
             c = atoi(argv[1] + 1);
